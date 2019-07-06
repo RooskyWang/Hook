7297036160cfa33a38b5e27af72c2121bec3fe49
龙飞 诸葛 <1561505155@qq.com>
2019-07-06 20:53:24 +0800

    阵型工具提交

---
+++ Assets/Scripts/Editor/FormationEditorInspector.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(FormationEditor))]
+public class FormationEditorInspector : Editor
+{
+	private FormationEditor formation;
+	private int createPosCount = 5;
+
+	private void OnEnable()
+	{
+		formation = serializedObject.targetObject as FormationEditor;
+	}
+
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+
+		EditorGUILayout.HelpBox("第一步：创建站位点并调整位置", MessageType.Info);
+		createPosCount = EditorGUILayout.IntField("创建位置点的数量", createPosCount);
+
+		if (GUILayout.Button("创建位置点"))
+		{
+			formation.CreatePos(createPosCount);
+		}
+
+		EditorGUILayout.HelpBox("第二步：创建测试单位，直观的看到站位点", MessageType.Info);
+		if (GUILayout.Button("创建展示单位【测试站位】"))
+		{
+			formation.CreateShowUnit();
+		}
+
+		EditorGUILayout.HelpBox("第三步：删除测试单位并保存prefab", MessageType.Info);
+		if (GUILayout.Button("删除展示单位"))
+		{
+			formation.DestoryShowUnit();
+		}
+	}
+}
---
+++ Assets/Scripts/Formation/FormationEditor.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FormationEditor : MonoBehaviour
+{
+	public GameObject showPrefab;
+
+	public void CreatePos(int count)
+	{
+		string name = "";
+		for (int i = 0; i < count; i++)
+		{
+			name = "Pos_" + i.ToString("D2");
+			if (transform.Find(name) == null)
+			{
+				GameObject obj = new GameObject();
+				obj.transform.parent = transform;
+				obj.transform.localPosition = Vector3.zero;
+				obj.transform.localScale = Vector3.one;
+				obj.transform.localRotation = Quaternion.identity;
+				obj.name = name;
+
+				obj.AddComponent<MapE_EnemyInfo>();
+			}
+		}
+	}
+
+	public void CreateShowUnit()
+	{
+		if (showPrefab == null)
+		{
+			Debug.LogError("没有设置要展示的单位预设【showPrefab】");
+			return;
+		}
+
+		DestoryShowUnit();
+
+		foreach (Transform child in transform)
+		{
+			if (child.name.StartsWith("Pos_"))
+			{
+				GameObject obj = Instantiate(showPrefab);
+				obj.transform.parent = child;
+				obj.transform.localPosition = Vector3.zero;
+				obj.transform.localScale = Vector3.one;
+				obj.transform.localRotation = Quaternion.identity;
+			}
+		}
+	}
+
+	public void DestoryShowUnit()
+	{
+		foreach (Transform child in transform)
+		{
+			if (child.name.StartsWith("Pos_"))
+			{
+				//清空子节点
+				foreach (Transform cc in child)
+				{
+					DestroyImmediate(cc.gameObject);
+				}
+			}
+		}
+	}
+}
